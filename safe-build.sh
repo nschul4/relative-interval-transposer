8ae009998faf3cc149cb4bd34ec26911f293fd9a
Neal Schultz <nschul4@github>
2026-08-07 21:14:58 +0100

    add midi-logger-vst to safe build

--- safe-build.sh
+++ safe-build.sh
@@ -4,6 +4,7 @@
 cd "$(dirname "$0")" || exit 1
 
 # Rebundle VST3 package in place
+cargo nih-plug bundle midi-logger-vst || exit 1
 cargo nih-plug bundle midi-transform-vst || exit 1
 
 echo "[OK] Successfully updated VST3 bundle in target/bundled."
